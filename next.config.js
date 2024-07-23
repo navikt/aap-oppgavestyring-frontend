908465fcb1345ee7d871a9626db9a5fbb3f83276
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-07-23 12:13:32 +0200

    Legger inn standalone build

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    trailingSlash: true,
+    reactStrictMode: true,
+    output: 'standalone',
+    assetPrefix: process.env.ASSET_PREFIX ?? undefined,
+  
+    experimental: {
+      instrumentationHook: true,
+      optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
+    },
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.node$/,
+        use: 'node-loader',
+      });
+  
+      return config;
+    },
+  
+    images: {
+      formats: ['image/webp'],
+    },
+  };
+  
+  module.exports = nextConfig;
