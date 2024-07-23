fa16e6576f7c672ff822a89534fba168013cf8af
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-07-23 14:37:04 +0200

    Tar i bruk azuread service fra felleslib

--- lib/utils/environment.ts
+++
@@ -1,7 +0,0 @@
-export const isLocal = () => {
-  return process.env.NEXT_PUBLIC_ENVIRONMENT === 'localhost';
-};
-
-export const isDev = () => {
-  return process.env.NEXT_PUBLIC_ENVIRONMENT === 'dev';
-};
