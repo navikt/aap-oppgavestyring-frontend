f1d5bfe19137e4ca16039346c9e2593c78d7ab07
Tor Idland <tor.idland@nav.no>
2024-10-02 13:33:24 +0200

    :children_crossing: Heading over åpne oppgaver-tabell

--- components/åpneoppgaver/ÅpneOppgaver.tsx
+++ components/åpneoppgaver/ÅpneOppgaver.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Table } from '@navikt/ds-react';
+import { Heading, Table } from '@navikt/ds-react';
 import { Oppgave } from '../../lib/types/types';
 
 interface Props {
@@ -9,15 +9,20 @@ interface Props {
 
 export const ÅpneOppgaver = ({ oppgaver }: Props) => {
   return (
-    <Table>
-      <Table.Header>
-        <Table.HeaderCell>Saksnummer</Table.HeaderCell>
-      </Table.Header>
-      {oppgaver.map((oppgave) => (
-        <Table.Row key={`${oppgave.id}`}>
-          <Table.DataCell>{oppgave.saksnummer}</Table.DataCell>
-        </Table.Row>
-      ))}
-    </Table>
+    <div>
+      <Heading level={'2'} size={'medium'}>
+        Mine oppgaver(NB: Under arbeid, trenger ikke testes)
+      </Heading>
+      <Table>
+        <Table.Header>
+          <Table.HeaderCell>Saksnummer</Table.HeaderCell>
+        </Table.Header>
+        {oppgaver.map((oppgave) => (
+          <Table.Row key={`${oppgave.id}`}>
+            <Table.DataCell>{oppgave.saksnummer}</Table.DataCell>
+          </Table.Row>
+        ))}
+      </Table>
+    </div>
   );
 };
