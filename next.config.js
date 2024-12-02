c79782807f7e67d02d4d5207934b59bea24d9b4b
Tor Idland <tor.idland@nav.no>
2024-12-02 15:36:37 +0100

    :wrench: konflikt med felles eslint config og instrumentation er ikke lenger experimental i next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
-    reactStrictMode: true,
-    output: 'standalone',
-    assetPrefix: process.env.ASSET_PREFIX ?? undefined,
-  
-    experimental: {
-      instrumentationHook: true,
-      optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.node$/,
-        use: 'node-loader',
-      });
-  
-      return config;
-    },
-  
-    images: {
-      formats: ['image/webp'],
-    },
-  };
-  
-  module.exports = nextConfig;
+  trailingSlash: true,
+  reactStrictMode: true,
+  output: 'standalone',
+  assetPrefix: process.env.ASSET_PREFIX ?? undefined,
+
+  experimental: {
+    optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'node-loader',
+    });
+
+    return config;
+  },
+
+  images: {
+    formats: ['image/webp'],
+  },
+};
+
+module.exports = nextConfig;
