4b55f84a9f7ce0267d013134caa53d9450fecc94
Tor Idland <tor.idland@nav.no>
2024-12-18 15:18:44 +0100

    :label: bruk publiserte typer og legg til endepunkt for å hente ut enheter

--- lib/services/oppgaveService/oppgaveService.ts
+++ lib/services/oppgaveService/oppgaveService.ts
@@ -1,12 +1,45 @@
 import { fetchProxy, fetchProxyNoRetry } from 'lib/services/fetchProxy';
-import { AvklaringsbehovReferanse, Kø, NesteOppgaveResponse, Oppgave } from 'lib/types/types';
+import { AvklaringsbehovReferanse, Enhet, Kø, NesteOppgaveResponse, Oppgave } from 'lib/types/types';
+import {
+  NoNavAapOppgaveOppgaveDtoBehandlingstype,
+  NoNavAapOppgaveOppgaveDtoStatus,
+} from '@navikt/aap-oppgave-typescript-types';
 
 const oppgaveApiBaseUrl = process.env.OPPGAVE_API_BASE_URL;
 const oppgaveApiScope = process.env.OPPGAVE_API_SCOPE ?? '';
 function isLocal() {
   return process.env.NEXT_PUBLIC_ENVIRONMENT === 'localhost';
 }
-
+const oppgaveMock = [
+  {
+    opprettetAv: 'tor',
+    avklaringsbehovKode: '5003',
+    behandlingOpprettet: '09-12-2024',
+    behandlingRef: `behandlingref-12`,
+    behandlingstype: NoNavAapOppgaveOppgaveDtoBehandlingstype.F_RSTEGANGSBEHANDLING,
+    endretTidspunkt: '10-12-2024',
+    id: 0,
+    opprettetTidspunkt: '09-12-2024',
+    saksnummer: `sak-234`,
+    status: NoNavAapOppgaveOppgaveDtoStatus.OPPRETTET,
+    versjon: 2,
+    enhet: 'HKLP',
+  },
+  {
+    opprettetAv: 'tor',
+    avklaringsbehovKode: '5003',
+    behandlingOpprettet: '09-12-2024',
+    behandlingRef: 'behandlingref-12',
+    behandlingstype: NoNavAapOppgaveOppgaveDtoBehandlingstype.KLAGE,
+    endretTidspunkt: '10-12-2024',
+    id: 0,
+    opprettetTidspunkt: '09-12-2024',
+    saksnummer: `sak-123`,
+    status: NoNavAapOppgaveOppgaveDtoStatus.OPPRETTET,
+    versjon: 2,
+    enhet: 'HFII',
+  },
+];
 export const hentKøer = async (): Promise<Kø[]> => {
   if (isLocal()) {
     return [
@@ -21,68 +54,14 @@ export const hentKøer = async (): Promise<Kø[]> => {
 };
 export const hentMineOppgaver = async (): Promise<Oppgave[]> => {
   if (isLocal()) {
-    return [
-      {
-        opprettetAv: 'tor',
-        avklaringsbehovKode: '5003',
-        behandlingOpprettet: '09-12-2024',
-        behandlingRef: 'behandlingref-12',
-        behandlingstype: 'FØRSTEGANGSBEHANDLING',
-        endretTidspunkt: '10-12-2024',
-        id: 0,
-        opprettetTidspunkt: '09-12-2024',
-        saksnummer: 'saksnummer-12',
-        status: 'OPPRETTET',
-        versjon: 2,
-      },
-      {
-        opprettetAv: 'tor',
-        avklaringsbehovKode: '5003',
-        behandlingOpprettet: '09-12-2024',
-        behandlingRef: 'behandlingref-12',
-        behandlingstype: 'KLAGE',
-        endretTidspunkt: '10-12-2024',
-        id: 0,
-        opprettetTidspunkt: '09-12-2024',
-        saksnummer: 'saksnummer-12',
-        status: 'OPPRETTET',
-        versjon: 2,
-      },
-    ];
+    return oppgaveMock;
   }
   const url = `${oppgaveApiBaseUrl}/mine-oppgaver`;
   return await fetchProxyNoRetry<Oppgave[]>(url, oppgaveApiScope, 'GET', undefined, 'oppgaveservice/mine-oppgaver');
 };
 export const hentAlleOppgaver = async (): Promise<Oppgave[]> => {
   if (isLocal()) {
-    return [
-      {
-        opprettetAv: 'tor',
-        avklaringsbehovKode: '5003',
-        behandlingOpprettet: '09-12-2024',
-        behandlingRef: 'behandlingref-12',
-        behandlingstype: 'FØRSTEGANGSBEHANDLING',
-        endretTidspunkt: '10-12-2024',
-        id: 0,
-        opprettetTidspunkt: '09-12-2024',
-        saksnummer: 'saksnummer-12',
-        status: 'OPPRETTET',
-        versjon: 2,
-      },
-      {
-        opprettetAv: 'tor',
-        avklaringsbehovKode: '5003',
-        behandlingOpprettet: '09-12-2024',
-        behandlingRef: 'behandlingref-12',
-        behandlingstype: 'KLAGE',
-        endretTidspunkt: '10-12-2024',
-        id: 0,
-        opprettetTidspunkt: '09-12-2024',
-        saksnummer: 'saksnummer-12',
-        status: 'OPPRETTET',
-        versjon: 2,
-      },
-    ];
+    return oppgaveMock;
   }
   const url = `${oppgaveApiBaseUrl}/alle-oppgaver`;
   return await fetchProxy<Oppgave[]>(url, oppgaveApiScope, 'GET');
@@ -95,26 +74,28 @@ export const hentOppgaverForFilter = async (filterId: number): Promise<Oppgave[]
         avklaringsbehovKode: '5003',
         behandlingOpprettet: '09-12-2024',
         behandlingRef: `behandlingref-12`,
-        behandlingstype: 'FØRSTEGANGSBEHANDLING',
+        behandlingstype: NoNavAapOppgaveOppgaveDtoBehandlingstype.F_RSTEGANGSBEHANDLING,
         endretTidspunkt: '10-12-2024',
         id: 0,
         opprettetTidspunkt: '09-12-2024',
         saksnummer: `sak-fra-kø-${filterId}-1`,
-        status: 'OPPRETTET',
+        status: NoNavAapOppgaveOppgaveDtoStatus.OPPRETTET,
         versjon: 2,
+        enhet: 'HKLP',
       },
       {
         opprettetAv: 'tor',
         avklaringsbehovKode: '5003',
         behandlingOpprettet: '09-12-2024',
         behandlingRef: 'behandlingref-12',
-        behandlingstype: 'KLAGE',
+        behandlingstype: NoNavAapOppgaveOppgaveDtoBehandlingstype.KLAGE,
         endretTidspunkt: '10-12-2024',
         id: 0,
         opprettetTidspunkt: '09-12-2024',
         saksnummer: `sak-fra-kø-${filterId}-2`,
-        status: 'OPPRETTET',
+        status: NoNavAapOppgaveOppgaveDtoStatus.OPPRETTET,
         versjon: 2,
+        enhet: 'HFII',
       },
     ];
   }
@@ -130,3 +111,15 @@ export const velgNesteOppgave = async (køId: number): Promise<NesteOppgaveRespo
   const url = `${oppgaveApiBaseUrl}/neste-oppgave`;
   return await fetchProxy<NesteOppgaveResponse>(url, oppgaveApiScope, 'POST', { filterId: køId });
 };
+
+export async function hentEnheter() {
+  if (isLocal()) {
+    return [
+      { enhetNr: 'FKSH', navn: 'Enhet en' },
+      { enhetNr: 'AHFG', navn: 'Enhet to' },
+      { enhetNr: 'HDMM', navn: 'Enhet tre' },
+    ];
+  }
+  const url = `${oppgaveApiBaseUrl}/enheter`;
+  return await fetchProxy<Array<Enhet>>(url, oppgaveApiScope, 'POST');
+}
--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,14 +1,20 @@
-import { components as oppgave } from 'lib/types/schema-oppgave';
-import { components as postmottak } from 'lib/types/schema-postmottak';
+import { components as oppgave } from '@navikt/aap-oppgave-typescript-types';
+import { components as postmottak } from '@navikt/aap-postmottak-backend-typescript-types';
 import { components as behandlingsflyt } from '@navikt/aap-behandlingsflyt-typescript-types';
 import { exhaustiveCheck } from 'lib/utils/typescript';
 
+// oppgave
 export type NesteOppgaveResponse = oppgave['schemas']['no.nav.aap.oppgave.plukk.NesteOppgaveDto'];
 export type Kø = oppgave['schemas']['no.nav.aap.oppgave.filter.FilterDto'];
 export type Oppgave = oppgave['schemas']['no.nav.aap.oppgave.OppgaveDto'];
 export type AvklaringsbehovReferanse = oppgave['schemas']['no.nav.aap.oppgave.AvklaringsbehovReferanseDto'];
+export type Enhet = oppgave['schemas']['no.nav.aap.oppgave.enhet.EnhetDto'];
+
+// behandlingsflyt
 export type BehandlingsFlytAvklaringsbehovKode =
   behandlingsflyt['schemas']['no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon']['kode'];
+
+// postmottak
 export type PostmottakAvklaringsbehovKode =
   postmottak['schemas']['no.nav.aap.postmottak.kontrakt.avklaringsbehov.Definisjon']['kode'];
 export type AvklaringsbehovKode = BehandlingsFlytAvklaringsbehovKode | PostmottakAvklaringsbehovKode;
